95ac48a6704cbbc93b473ecc8d0245eecdb6dd7b
¨jppinedal¨ <jppinedal@unal.edu.co>
2025-12-01 15:31:26 -0500

    entrega lab6

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,7 +1,8 @@
 """
 Escriba el codigo que ejecute la accion solicitada en la pregunta.
 """
-
+import os
+import pandas as pd
 
 def pregunta_01():
     """
@@ -13,3 +14,48 @@ def pregunta_01():
     El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
 
     """
+    df = pd.read_csv(
+        "files/input/solicitudes_de_credito.csv",
+        sep=";",
+        index_col=0
+    )
+    # 1. Limpieza de columnas tipo texto (object)
+    text_cols = df.select_dtypes(include=["object"]).columns
+
+    for col in text_cols:
+        df[col] = (
+            df[col]
+            .str.lower()
+            .str.replace("_", " ", regex=False)
+            .str.replace("-", " ", regex=False)
+            .str.replace(",", "", regex=False)
+            .str.replace("$", "", regex=False)
+            .str.replace(".00", "", regex=False)
+        )
+    #2Conversión de tipos numéricos
+    df["monto_del_credito"] = df["monto_del_credito"].astype(float)
+    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
+
+    # 3. Corrección del formato de fecha
+    #    Algunos registros están en formato dd/mm/YYYY, otros YYYY/mm/dd
+    fecha = df["fecha_de_beneficio"]
+
+    fecha_1 = pd.to_datetime(fecha, format="%d/%m/%Y", errors="coerce")
+    fecha_2 = pd.to_datetime(fecha, format="%Y/%m/%d", errors="coerce")
+
+    # Combina el que sirva (el primero no nulo)
+    df["fecha_de_beneficio"] = fecha_1.combine_first(fecha_2)
+    # 4. Eliminación de duplicados y na
+    df = df.drop_duplicates()
+    df = df.dropna()
+    # 5. Guardado del resultado
+    os.makedirs("files/output", exist_ok=True)
+
+    df.to_csv(
+        "files/output/solicitudes_de_credito.csv",
+        index=False,
+        sep=";",
+        encoding="utf-8"
+    )
+     
+
